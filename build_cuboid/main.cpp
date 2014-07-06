88aa78a10bb711f704c7d9c60c9228590c6806fe
Frankbau <frank.bauernoeppel@htw-berlin.de>
2014-07-05 18:08:00 -0700

    zurich 3 layer cuboid generated

--- build_cuboid/main.cpp
+++ build_cuboid/main.cpp
@@ -30,15 +30,15 @@ int main(int argc, char * argv)
 
 	file << (x + 1)*(y + 1)*(z + 1) << " 3 0 0" << endl;
 
-	for (int i = 0; i <= x; ++i)
+	for (int xx = 0; xx <= x; ++xx)
 	{
-		for (int j = 0; j <= y; ++j)
+		for (int yy = 0; yy <= y; ++yy)
 		{
-			for (int k = 0; k <= z; ++k)
+			for (int zz = 0; zz <= z; ++zz)
 			{
 				// Node index, node coordinates
-				int node_index = 1 + i*(y + 1)*(z + 1) + j*(z + 1) + k;
-				file << node_index << " " << dx*i << " " << dy*j << " " << dz*k << endl;
+				int node_index = 1 + xx*(y + 1)*(z + 1) + yy*(z + 1) + zz;
+				file << node_index << " " << dx*xx << " " << dy*yy << " " << -(dz*zz) << endl; // z axis goes down
 			}
 		}
 	}
@@ -49,19 +49,19 @@ int main(int argc, char * argv)
 
 	// facets
 	// perpendicular to x axis
-	for (int i = 0; i <= x; ++i)
+	for (int xx = 0; xx <= x; ++xx)
 	{
-		for (int j = 0; j < y; ++j)
+		for (int yy = 0; yy < y; ++yy)
 		{
-			for (int k = 0; k < z; ++k)
+			for (int zz = 0; zz < z; ++zz)
 			{
 				// 1 polygon, no hole, no boundary marker
 				file << "1" << endl;
 
-				int node1 = 1 + i*(y + 1)*(z + 1) + (j + 0)*(z + 1) + (k + 0);
-				int node2 = 1 + i*(y + 1)*(z + 1) + (j + 1)*(z + 1) + (k + 0);
-				int node3 = 1 + i*(y + 1)*(z + 1) + (j + 1)*(z + 1) + (k + 1);
-				int node4 = 1 + i*(y + 1)*(z + 1) + (j + 0)*(z + 1) + (k + 1);
+				int node1 = 1 + xx*(y + 1)*(z + 1) + (yy + 0)*(z + 1) + (zz + 0);
+				int node2 = 1 + xx*(y + 1)*(z + 1) + (yy + 1)*(z + 1) + (zz + 0);
+				int node3 = 1 + xx*(y + 1)*(z + 1) + (yy + 1)*(z + 1) + (zz + 1);
+				int node4 = 1 + xx*(y + 1)*(z + 1) + (yy + 0)*(z + 1) + (zz + 1);
 
 				file << "4 " << node1 << " " << node2 << " " << node3 << " " << node4 << endl;
 			}
@@ -69,19 +69,19 @@ int main(int argc, char * argv)
 	}
 
 	// perpendicular to y axis
-	for (int i = 0; i < x; ++i)
+	for (int xx = 0; xx < x; ++xx)
 	{
-		for (int j = 0; j <= y; ++j)
+		for (int yy = 0; yy <= y; ++yy)
 		{
-			for (int k = 0; k < z; ++k)
+			for (int zz = 0; zz < z; ++zz)
 			{
 				// 1 polygon, no hole, no boundary marker
 				file << "1" << endl;
 
-				int node1 = 1 + (i + 0)*(y + 1)*(z + 1) + j*(z + 1) + (k + 0);
-				int node2 = 1 + (i + 1)*(y + 1)*(z + 1) + j*(z + 1) + (k + 0);
-				int node3 = 1 + (i + 1)*(y + 1)*(z + 1) + j*(z + 1) + (k + 1);
-				int node4 = 1 + (i + 0)*(y + 1)*(z + 1) + j*(z + 1) + (k + 1);
+				int node1 = 1 + (xx + 0)*(y + 1)*(z + 1) + yy*(z + 1) + (zz + 0);
+				int node2 = 1 + (xx + 1)*(y + 1)*(z + 1) + yy*(z + 1) + (zz + 0);
+				int node3 = 1 + (xx + 1)*(y + 1)*(z + 1) + yy*(z + 1) + (zz + 1);
+				int node4 = 1 + (xx + 0)*(y + 1)*(z + 1) + yy*(z + 1) + (zz + 1);
 
 				file << "4 " << node1 << " " << node2 << " " << node3 << " " << node4 << endl;
 			}
@@ -89,19 +89,19 @@ int main(int argc, char * argv)
 	}
 
 	// perpendicular to z axis
-	for (int i = 0; i < x; ++i)
+	for (int xx = 0; xx < x; ++xx)
 	{
-		for (int j = 0; j < y; ++j)
+		for (int yy = 0; yy < y; ++yy)
 		{
-			for (int k = 0; k <= z; ++k)
+			for (int zz = 0; zz <= z; ++zz)
 			{
 				// 1 polygon, no hole, no boundary marker
 				file << "1" << endl;
 
-				int node1 = 1 + (i + 0)*(y + 1)*(z + 1) + (j + 0)*(z + 1) + k;
-				int node2 = 1 + (i + 1)*(y + 1)*(z + 1) + (j + 0)*(z + 1) + k;
-				int node3 = 1 + (i + 1)*(y + 1)*(z + 1) + (j + 1)*(z + 1) + k;
-				int node4 = 1 + (i + 0)*(y + 1)*(z + 1) + (j + 1)*(z + 1) + k;
+				int node1 = 1 + (xx + 0)*(y + 1)*(z + 1) + (yy + 0)*(z + 1) + zz;
+				int node2 = 1 + (xx + 1)*(y + 1)*(z + 1) + (yy + 0)*(z + 1) + zz;
+				int node3 = 1 + (xx + 1)*(y + 1)*(z + 1) + (yy + 1)*(z + 1) + zz;
+				int node4 = 1 + (xx + 0)*(y + 1)*(z + 1) + (yy + 1)*(z + 1) + zz;
 
 				file << "4 " << node1 << " " << node2 << " " << node3 << " " << node4 << endl;
 			}
@@ -112,7 +112,34 @@ int main(int argc, char * argv)
 	file << "0" << endl; // no holes
 
 	// Part 3 - region list
+#if 0
+	// number of regions
 	file << "0" << endl; // no regions (yet)
+#else
+	// number of regions
+	file << x*y*z << endl;
+
+	for (int xx = 0; xx < x; ++xx)
+	{
+		for (int yy = 0; yy < y; ++yy)
+		{
+			for (int zz = 0; zz < z; ++zz)
+			{
+				int region_index = 1 + xx*y*z + yy*z + zz;
+				
+				double weight;
+				if (zz >= 30)
+					weight = 5000;
+				else if (zz >= 10)
+					weight = 2000;
+				else
+					weight = 500;
+
+				file << region_index << " " << dx*xx+dx/2 << " " << dy*yy+dy/2 << " " << -(dz*zz+dz/2) << " " << weight << endl;
+			}
+		}
+	}
+#endif
 
 	return EXIT_SUCCESS;
 }
