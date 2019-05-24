d417732f5c1d2d9f2fbf68233cc06a17bb04739a
Frank Bauernöppel <frank@bauernoeppel.de>
2019-05-24 07:28:41 +0200

    changed to length factor 4 (from 3)

--- build_cuboid/main.cpp
+++ build_cuboid/main.cpp
@@ -6,168 +6,7 @@
 
 using namespace std;
 
-#if 0
-
-// small test cuboid
-int x_min = 0;	// number of cubes in x direction (left to right)
-int y_min = 0;		// number of cubes in y direction
-int z_min = 0;		// number of cubes in z direction (down)
-
-int x_max = 2;	// number of cubes in x direction (left to right)
-int y_max = 3;		// number of cubes in y direction
-int z_max = 4;		// number of cubes in z direction (down)
-
-#else
-
-int x_min = -3*8;	// number of cubes in x direction (left to right)
-int y_min = -8*3;		// number of cubes in y direction
-int z_min = -144/6;		// number of cubes in z direction (down)
-
-int x_max = 3*8;	// number of cubes in x direction (left to right)
-int y_max = 8*3;		// number of cubes in y direction
-int z_max = 144/6;		// number of cubes in z direction (down)
-
-#endif
-
-// cube edge length
-double dx = 1.0/8;
-double dy = 1.0/3;
-double dz = 1.0*6;
-
-string filename = "cuboid.poly";
-
-// linearized index of a 3D node
-int idx(int x, int y, int z)
-{
-	return (((x-x_min) * (y_max - y_min + 1)) + (y-y_min)) * (z_max - z_min + 1) + (z-z_min) + 1;
-}
-
-int weight(int x, int y, int z)
-{
-	if (z > z_min+(z_max-z_min)/2)
-		return 4;
-	else
-		return 1;
-}
-
-int main1(int argc, char * argv)
-{
-
-	ofstream file(filename, ios::trunc);
-	if (!file.is_open())
-	{
-		cerr << "failed to open file " << filename << endl;
-		return EXIT_FAILURE;
-	}
-
-	// Part 1 - node list
-	// node count, 3 dim, no attribute, no boundary marker
-
-	file << ((x_max-x_min) + 1)*((y_max-y_min) + 1)*((z_max-z_min) + 1) << " 3 0 0" << endl;
-
-	for (int xx = x_min; xx <= x_max; ++xx)
-	{
-		for (int yy = y_min; yy <= y_max; ++yy)
-		{
-			for (int zz = z_min; zz <= z_max; ++zz)
-			{
-				// Node index, node coordinates
-				int node_index = idx(xx, yy, zz);
-				file << node_index << " " << dx*xx << " " << dy*yy << " " << dz*zz << endl; // z axis goes down
-			}
-		}
-	}
-
-	// Part 2 - facet list
-	// facet count, no boundary marker
-	file << ((x_max - x_min) + 1)* (y_max - y_min) * (z_max - z_min) + (x_max - x_min) *((y_max - y_min) + 1)* (z_max - z_min) + (x_max - x_min) * (y_max - y_min) *((z_max - z_min) + 1) << " 0" << endl;
-
-	// facets
-	// perpendicular to x axis
-	for (int xx = x_min; xx <= x_max; ++xx)
-	{
-		for (int yy = y_min; yy < y_max; ++yy)
-		{
-			for (int zz = z_min; zz < z_max; ++zz)
-			{
-				// 1 polygon, no hole, no boundary marker
-				file << "1" << endl;
-
-				int node1 = idx(xx, yy + 0, zz + 0);
-				int node2 = idx(xx, yy + 1, zz + 0);
-				int node3 = idx(xx, yy + 1, zz + 1);
-				int node4 = idx(xx, yy + 0, zz + 1);
-
-				file << "4 " << node1 << " " << node2 << " " << node3 << " " << node4 << endl;
-			}
-		}
-	}
-
-	// perpendicular to y axis
-	for (int xx = x_min; xx < x_max; ++xx)
-	{
-		for (int yy = y_min; yy <= y_max; ++yy)
-		{
-			for (int zz = z_min; zz < z_max; ++zz)
-			{
-				// 1 polygon, no hole, no boundary marker
-				file << "1" << endl;
-
-				int node1 = idx(xx + 0, yy, zz + 0);
-				int node2 = idx(xx + 1, yy, zz + 0);
-				int node3 = idx(xx + 1, yy, zz + 1);
-				int node4 = idx(xx + 0, yy, zz + 1);
-
-				file << "4 " << node1 << " " << node2 << " " << node3 << " " << node4 << endl;
-			}
-		}
-	}
-
-	// perpendicular to z axis
-	for (int xx = x_min; xx < x_max; ++xx)
-	{
-		for (int yy = y_min; yy < y_max; ++yy)
-		{
-			for (int zz = z_min; zz <= z_max; ++zz)
-			{
-				// 1 polygon, no hole, no boundary marker
-				file << "1" << endl;
-
-				int node1 = idx(xx + 0, yy + 0, zz);
-				int node2 = idx(xx + 1, yy + 0, zz);
-				int node3 = idx(xx + 1, yy + 1, zz);
-				int node4 = idx(xx + 0, yy + 1, zz);
-
-				file << "4 " << node1 << " " << node2 << " " << node3 << " " << node4 << endl;
-			}
-		}
-	}
-
-	// Part 3 - hole list
-	file << "0" << endl; // no holes
-
-	// Part 3 - region list
-	// number of regions, each center of a cube defines its own region
-	file << (x_max-x_min) * (y_max-y_min) * (z_max-z_min) << endl; // no holes
-
-	for (int xx = x_min; xx < x_max; ++xx)
-	{
-		for (int yy = y_min; yy < y_max; ++yy)
-		{
-			for (int zz = z_min; zz < z_max; ++zz)
-			{
-				int node_index = idx(xx, yy, zz);
-
-				// center coord
-				file << node_index << " " << dx * (xx+0.5) << " " << dy * (yy+0.5) << " " << dz * (zz+0.5) << " " << weight(xx,yy,zz) << endl; // z axis goes down
-			}
-		}
-	}
-
-	return EXIT_SUCCESS;
-}
-
-/////////////////////////////// generation with open volume mesh ? //////////////////////
+/////////////////////////////// generation with open volume mesh //////////////////////
 
 #define INCLUDE_TEMPLATES
 #include <OpenVolumeMesh/Geometry/VectorT.hh>
@@ -362,150 +201,79 @@ struct Mesh : GeometricHexahedralMeshV3f
 		}
 		this->weight[ch] = weight;
 	}
+
+	void generate(int n, string filename)
+	{
+		const int wx = 1;
+		const int wy = 4 * n;
+		const int wz = 4 * 4 * wy * n;
+
+		// weight=16: center
+		// these are n large slabs of height 1 and weight 1
+		// they are only partitioned into smaller parts to match satellite cuboid sizes
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				for (int k = 0; k < n; ++k) {
+					add_cuboid(i * wx, j * wy, k * wz, wx, wy, wz, 16);
+				}
+			}
+		}
+
+		// weight=4: top-right and bottom-left
+		for (int j = 0; j < n; ++j) {
+			for (int k = 0; k < n; ++k) {
+				if (j % 2) {
+					add_cuboid( n, j * wy, k * wz,    +1, wy, wz,    4);
+				}
+				else {
+					add_cuboid( 0, j * wy, k * wz,    -1, wy, wz,    4);
+				}
+			}
+		}
+
+		for (int i = 0; i < n; ++i) {
+			for (int k = 0; k < n; ++k) {
+				if (k % 2) {
+					add_cuboid(i * wx, n * wy, k * wz, wx, 1, wz, 1);
+				}
+				else {
+					add_cuboid(i * wx,      0, k * wz, wx, -1, wz, 1);
+				}
+			}
+		}
+
+		// weight=1: front-left and rear-right
+
+		cout << " n_cells=" << n_cells();
+		cout << " n_faces=" << n_faces();
+		cout << " n_edges=" << n_edges();
+		cout << " n_vertices=" << n_vertices();
+		cout << " genus=" << genus();
+		cout << endl;
+
+		write_poly(filename);
+	}
+
+
 };
-
-
-/////////////////////////////////////////// d=3 n=2 ////////////////////////////////////
-int main1()
-{
-	Mesh mesh;
-	
-	int w = 1;
-
-	// center - weight 1
-	mesh.add_cuboid( 0, 0, 0, 1,  6,  144, 16 );
-	mesh.add_cuboid(-1, 0, 0, 1,  6,  144, 16 );
-	mesh.add_cuboid( 0, 0, 0, 1,  6, -144, 16 );
-	mesh.add_cuboid(-1, 0, 0, 1,  6, -144, 16 );
-	mesh.add_cuboid( 0, 0, 0, 1, -6,  144, 16 );
-	mesh.add_cuboid(-1, 0, 0, 1, -6,  144, 16 );
-	mesh.add_cuboid( 0, 0, 0, 1, -6, -144, 16 );
-	mesh.add_cuboid(-1, 0, 0, 1, -6, -144, 16 );
-
-	// green: top-right and bottom-left
-	mesh.add_cuboid( 1, 0, 0,  2, -6,  144, 4 );
-	mesh.add_cuboid( 1, 0, 0,  2, -6, -144, 4 );
-	mesh.add_cuboid(-1, 0, 0, -2,  6,  144, 4 );
-	mesh.add_cuboid(-1, 0, 0, -2,  6, -144, 4 );
-
-	// gold: front-left and rear-right
-	mesh.add_cuboid(-1, 8, 0, 1, -2, 144, 1 );
-	mesh.add_cuboid( 0, 8, 0, 1, -2, 144, 1 );
-
-	mesh.add_cuboid(1, -8, 0, -1, 2, -144, 1 );
-	mesh.add_cuboid(0, -8, 0, -1, 2, -144, 1 );
-
-
-	cout << " n_cells=" << mesh.n_cells();
-	cout << " n_faces=" << mesh.n_faces();
-	cout << " n_edges=" << mesh.n_edges();
-	cout << " n_vertices=" << mesh.n_vertices();
-	cout << " genus=" << mesh.genus();
-	cout << endl;
-
-	mesh.write_poly("test.poly");
-
-	// preview (coarse): ..\..\tetgen.exe -pqA test.poly
-	// finer: ..\..\tetgen.exe -pqAa0.1 test.poly
-	// finest: ..\..\tetgen.exe -pq1.3Aa0.01 test.poly (Mesh tetrahedra: 3810112)
-
-	// generates: .node .ele. face .edge
-
-	// reads .node and .ele, ignores others
-	// ..\x64\RelWithDebInfo\wsp3dovm.exe --input-mesh test.1  --write_mesh_vtk 1 --start_vertex 1 --termination_vertex 12
-
-	return 0;
-}
 
 
 int main()
 {
 	Mesh mesh;
+	
+	mesh.generate( 4, "D:/Carleton/2019/d3n4/d3n4.poly" );
 
-	int n = 4;
-	const int wx = 1;
-	const int wy = 3 * n;
-	const int wz = 4 * 3 * wy * n;
-
-#if 1
-	// weight=16: center
-	// these are n large slabs of height 1 and weight 1
-	// they are only partitioned into smaller parts to match satellite cuboid sizes
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			for (int k=0; k < n; ++k) {
-				mesh.add_cuboid( i*wx, j*wy, k*wz, wx, wy, wz, 16 );
-			}
-		}
-	}
-#endif
-
-#if 1
-	// weight=4: top-right and bottom-left
-	for (int j = 0; j < n; ++j) {
-		for (int k = 0; k < n; ++k) {
-			if (j % 2) {
-				mesh.add_cuboid( n, j * wy, k * wz, 1, wy, wz, 4);
-			}
-			else {
-				mesh.add_cuboid( 0, j * wy, k * wz, -1, wy, wz, 4);
-			}
-		}
-	}
-#endif
-
-	for (int i = 0; i < n; ++i) {
-		for (int k = 0; k < n; ++k) {
-			if (k % 2) {
-				mesh.add_cuboid( i*wx, n * wy, k * wz, wx, 1, wz, 1);
-			}
-			else {
-				mesh.add_cuboid( i*wx, 0, k * wz, wx, -1, wz, 1);
-			}
-		}
-	}
-
-	// weight=1: front-left and rear-right
-
-#if 0
-	mesh.add_cuboid(0, 0, 0, 1, 6, 144, 16);
-	mesh.add_cuboid(-1, 0, 0, 1, 6, 144, 16);
-	mesh.add_cuboid(0, 0, 0, 1, 6, -144, 16);
-	mesh.add_cuboid(-1, 0, 0, 1, 6, -144, 16);
-	mesh.add_cuboid(0, 0, 0, 1, -6, 144, 16);
-	mesh.add_cuboid(-1, 0, 0, 1, -6, 144, 16);
-	mesh.add_cuboid(0, 0, 0, 1, -6, -144, 16);
-	mesh.add_cuboid(-1, 0, 0, 1, -6, -144, 16);
-
-	// green: top-right and bottom-left
-	mesh.add_cuboid(1, 0, 0, 2, -6, 144, 4);
-	mesh.add_cuboid(1, 0, 0, 2, -6, -144, 4);
-	mesh.add_cuboid(-1, 0, 0, -2, 6, 144, 4);
-	mesh.add_cuboid(-1, 0, 0, -2, 6, -144, 4);
-
-	// gold: front-left and rear-right
-	mesh.add_cuboid(-1, 8, 0, 1, -2, 144, 1);
-	mesh.add_cuboid(0, 8, 0, 1, -2, 144, 1);
-
-	mesh.add_cuboid(1, -8, 0, -1, 2, -144, 1);
-	mesh.add_cuboid(0, -8, 0, -1, 2, -144, 1);
-#endif
-
-	cout << " n_cells=" << mesh.n_cells();
-	cout << " n_faces=" << mesh.n_faces();
-	cout << " n_edges=" << mesh.n_edges();
-	cout << " n_vertices=" << mesh.n_vertices();
-	cout << " genus=" << mesh.genus();
-	cout << endl;
-
-	mesh.write_poly("test.poly");
-
-	// ..\..\tetgen.exe -pqA test.poly (Mesh tetrahedra: 2106337)
-
+	// ..\..\tetgen1.5.0\x64\Release\tetgen.exe -pA D:\Carleton\2019\d3n4\d3n4.poly
+	// or: ..\..\tetgen1.5.0\x64\Release\tetgen.exe -pqAV D:\Carleton\2019\d3n4\d3n4.poly
 	// generates: .node .ele. face .edge
 
 	// reads .node and .ele, ignores others
-	// ..\x64\RelWithDebInfo\wsp3dovm.exe --input-mesh test.1  --write_mesh_vtk 1 --start_vertex 60 --termination_vertex 64
+
+	// wsp on left face:
+	// ..\x64\RelWithDebInfo\wsp3dovm.exe --input-mesh D:\Carleton\2019\d3n4\d3n4.1 --write_mesh_vtk 1 --start_vertex 49 --termination_vertex 49
+
+	// ..\x64\RelWithDebInfo\wsp3dovm.exe --input-mesh test.1  --write_mesh_vtk 1 
 
 	return 0;
 }
